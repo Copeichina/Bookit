b7ef3bbed63c412136c9cac04a8e84c9512d110f
Copeichina <57867741+Copeichina@users.noreply.github.com>
2020-02-29 14:57:57 -0500

    Update SecondClass.java

--- src/Test/SecondClass.java
+++ src/Test/SecondClass.java
@@ -3,5 +3,8 @@ package Test;
 public class SecondClass {
     public static void main(String[] args) {
         System.out.println("Push the code only for this class");
+        
+        
+         System.out.println("Adition the file in GitHub");    
     }
 }
